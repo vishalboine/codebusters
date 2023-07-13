dad4e0ceb40ac3f8ae55724308fbb618d6d1f0eb
Faisal Sayyed <sayyedfaisal06@gmail.com>
2023-07-13 20:34:54 +0530

    made changes for login page

--- frontend/src/pages/auth/login/Login.tsx
+++ frontend/src/pages/auth/login/Login.tsx
@@ -1,12 +1,56 @@
+import { useState, useCallback } from "react"
+import LongLogo from "../../../assets/images/longLogo.png";
+import image1 from "../../../assets/images/image1.png";
+import { LOGO_ALT } from "../../../constants";
 import "./Login.scss"
+import Input from "../../../components/ui-widgets/Input/Input";
+import Button from "../../../components/ui-widgets/Button/Button";
 
 type Props = {}
 
 const Login = (props: Props) => {
 
+  const [togglePassword, setTogglePassword] = useState(false);
+
+  const changePasswordVisibility = () => {
+    setTogglePassword((prev) => !prev)
+  }
+  console.log(togglePassword);
+  
   return (
     <div className='authBg'>
-      login page
+      <div className="loginWrapper">
+        <div className="loginCard">
+          <img src={LongLogo} alt={LOGO_ALT} className="authLogo" />
+          <div className="loginWrapper_main">
+            <div className="left">
+              <img src={image1} alt="" />
+            </div>
+            <div className="right">
+
+              <div className="input_div">
+                <h2>Login Now</h2>
+                <Input
+                  placeholder="Enter e-mail id"
+                  label="E-mail"
+                  />
+                <Input
+                  placeholder="Enter password"
+                  label="Password"
+                  showIcon
+                  type={togglePassword ? 'text' : 'password'}
+                  isVisiblePassword={togglePassword}
+                  onIconClick={changePasswordVisibility}
+                />
+                <div style={{ marginTop: '10px' }}>
+                  <input type="checkbox" name="forget_password" id="forget_password" /> <label htmlFor="forget_password">Forget password ?</label>
+                </div>
+                <Button title="Login" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
