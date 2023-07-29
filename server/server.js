4482ded5701ffa683cf1181d65d46c7408b6ee7e
Vishal Boine <vishal.b@orosoftsolutions.com>
2023-07-30 01:39:44 +0530

    Allow all methods to pass Api

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
          res.setHeader('Access-Control-Allow-Origin', origin);
     }
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
