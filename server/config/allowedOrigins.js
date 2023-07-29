f3c229110d40b0731d64d767c6693ac93d1eb891
Vishal Boine <vishal.b@orosoftsolutions.com>
2023-07-29 21:25:31 +0530

    browser URL added to hit API

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,8 +2,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://codebusters.neofinancials.com', 
-    'http://codebusters.neofinancials.com/api' 
+    'http://codebusters.neofinancials.com',
+    'http://codebusters.neofinancials.com/',
 ];
 
 module.exports = allowedOrigins;
