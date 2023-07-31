473e320e50d800d58ce1cd7c4833276a86f9e49b
Vishal Boine <vishal.b@orosoftsolutions.com>
2023-07-31 12:22:08 +0530

    api endpoint added wuth https

--- frontend/src/config/axiosInstance.ts
+++ frontend/src/config/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://codebusters.neofinancials.com/api';
 
 export default axios.create({
     baseURL: BASE_URL
