366587c910fdc7d37801493cc92f34ecb9780fa5
Vishal Boine <vishal.b@orosoftsolutions.com>
2023-08-06 23:57:36 +0530

    Val to choose at least on mapper

--- frontend/src/utils/common.ts
+++ frontend/src/utils/common.ts
@@ -72,7 +72,7 @@ function getObjectValueTypes(obj: any) {
   function compareValues(json1 : any, json2 : any, mapping : any) {
     const obj : any= {};
       Object.entries(mapping).forEach(([key1, key2] : any) => {
-        if(json1[key1].toLowerCase() === json2[key2].toLowerCase()){
+        if(json1[key1]?.toLowerCase() === json2[key2]?.toLowerCase()){
             obj[key1] = true;
         }else{
             obj[key1] = false;
