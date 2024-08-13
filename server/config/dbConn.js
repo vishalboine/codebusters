be171437558486b047a59f484b0988129d43ee9e
Vishal Boine <vishal.b@orosoftsolutions.com>
2024-08-13 18:21:16 +0530

    db change

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     mongoose.set('strictQuery', true);
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
+        await mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser:true,useUnifiedTopology:true});
     } catch (err) {
         console.error(err);
     }
